94941bb1e436279199acbdaa58fb8b04e36f767e
Italo <soarescmpos@gmail.com>
2023-06-22 15:51:32 -0300

    Fixed Ref error in CartButton component

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,13 +1,18 @@
+// EU CONSIDERO QUE CHEGUEI NUMA SOLUÇÃO MEIO RUIM MAS TUDO BEM
+// SE VOCÊ LEU ISSO ME DIZ SE É RUIM OU NÃO E SE MELHORARIA ALGO, POR FAVOR
+
+import React, { forwardRef, ComponentProps } from 'react'
 import { Handbag } from 'phosphor-react'
 import { CartButtonContainer } from './styles'
-import { ComponentProps } from 'react'
 
 type CartButtonProps = ComponentProps<typeof CartButtonContainer>
 
-export function CartButton({ ...rest }: CartButtonProps) {
-  return (
-    <CartButtonContainer {...rest}>
-      <Handbag weight="bold" />
-    </CartButtonContainer>
-  )
-}
+export const CartButton = forwardRef<HTMLButtonElement, CartButtonProps>(
+  function CartButton({ ...rest }, ref) {
+    return (
+      <CartButtonContainer ref={ref} {...rest}>
+        <Handbag weight="bold" />
+      </CartButtonContainer>
+    )
+  },
+)
